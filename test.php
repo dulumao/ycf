9b2cfc75ad2de4c47e719684f1f44cd88632bb9f
kcloze <lixiaopei@yaochufa.com>
2016-01-07 10:31:06 +0800

    test

--- test.php
+++ test.php
@@ -3,8 +3,8 @@ require_once('fastdfs.php');
 
 $fdfs = new FDFS();
 
-$localfile = 'test.xml';
-$fileinfo = $fdfs->upload($localfile,'xml');
+$localfile = 'test.json';
+$fileinfo = $fdfs->upload($localfile,'json');
 
 if ($fileinfo) {
   // update file info in the database etc
