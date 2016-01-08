71377a7329af453e2410d7c2977483388ffddd94
kcloze <lixiaopei@yaochufa.com>
2016-01-08 15:02:40 +0800

    download file

--- benchmark.php
+++ benchmark.php
@@ -6,12 +6,15 @@ $fdfs = new FDFS();
 
 $localfile = $_GET['fname']?$_GET['fname']:'001';
 $fileinfo = $fdfs->upload('upload/'.$localfile,'');
-$fdfs->closs();//一定要释放连接
+
+
 if ($fileinfo) {
+    $fdfs->download_to_buff($fileinfo['group_name',$fileinfo['remote_filename']);
     echo 'true';
 }else{
     echo 'false';
 }
+$fdfs->closs();//一定要释放连接
 
 
 
