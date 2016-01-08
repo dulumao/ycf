e8e88250b847e99e3090a7db79d6c3b2550004d5
kcloze <lixiaopei@yaochufa.com>
2016-01-08 09:57:19 +0800

    test

--- test.php
+++ test.php
@@ -6,7 +6,7 @@ $fdfs = new FDFS();
 
 $localfile = $_GET['fname']?$_GET['fname']:'001';
 $fileinfo = $fdfs->upload('upload/'.$localfile,'');
-$fdfs->closs();//一定要释放连接
+
 if ($fileinfo) {
   // update file info in the database etc
 }else{
@@ -14,4 +14,4 @@ if ($fileinfo) {
 }
 
 var_dump($fileinfo);
-
+$fdfs->closs();//一定要释放连接
