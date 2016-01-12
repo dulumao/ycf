2e073474446894bdce96f466ab5431806076a926
kcloze <lixiaopei@yaochufa.com>
2016-01-12 12:45:46 +0800

    init

--- include/FileFilter.php
+++ include/FileFilter.php
@@ -5,7 +5,7 @@ class FileFilter{
 
     public static function checkExtName($file){
         // 如果是图像文件 检测文件格式
-        if( in_array(strtolower($file['extension']),array('gif','jpg','jpeg','bmp','png','swf'))) {
+        if(isset($file['extension']) && in_array(strtolower($file['extension']),array('gif','jpg','jpeg','bmp','png','swf'))) {
             $info   = getimagesize($file['tmp_name']);
             if(false === $info || ('gif' == strtolower($file['extension']) && empty($info['bits']))){
                 self::$error = '非法图像文件';
