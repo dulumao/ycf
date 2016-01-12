ba36a5b39abcc566c2cae1b74ddd7e4c2c2f8dc3
kcloze <lixiaopei@yaochufa.com>
2016-01-12 17:35:30 +0800

    init

--- include/YcfUpload.php
+++ include/YcfUpload.php
@@ -29,6 +29,7 @@ class YcfUpload {
                     $fileinfo = $fdfs->upload($origin_file_name,$extensin_name);
 
                     if ($fileinfo) {
+                        $fileinfo['remote_url']=$fileinfo['group_name'].'/'.$fileinfo['remote_filename'];
                         echo json_encode(array('code'=>100,'message'=>'success','content'=>$fileinfo));
                         //$result=$fdfs->download_to_buff($fileinfo['group_name'],$fileinfo['remote_filename']);
                         //var_dump($result);
