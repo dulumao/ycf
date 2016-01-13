54640028971e981437c73ed622cbe2676e5141d3
kcloze <pei.greet@gmail.com>
2016-01-14 01:31:55 +0800

    init

--- service/YcfPdo.php
+++ service/YcfPdo.php
@@ -4,7 +4,7 @@ class YcfPdo extends YcfCore{
 
 
 	public static function actionTest(){
-		$data['pNamewe']='fww';
+		$data['pName']='fww';
 		$data['pValue']='总过万佛无法';
         $insert = self::$_db->query("INSERT INTO pdo_test( pName,pValue) VALUES ( :pName,:pValue)", $data);
         if($insert > 0 ) {
