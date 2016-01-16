608314b8a67b98140755a650d6101dbfefebeed3
kcloze <pei.greet@gmail.com>
2016-01-17 01:22:20 +0800

    redis

--- src/Service/YcfPdo.php
+++ src/Service/YcfPdo.php
@@ -1,20 +1,15 @@
 <?php
 namespace Ycf\Service;
-
-use Ycf\Model\ModelTest;
+use Ycf\Model\ModelPdo;
 
 class YcfPdo {
 
 	public function actionTest() {
-		$modelTest = new ModelTest();
+		$modelTest = new ModelPdo();
 		$result = $modelTest->testInsert();
 		var_dump($result);
 
 		$result = $modelTest->testQuery();
-		var_dump($result);
-
-		$result = $modelTest->testRedis();
-		var_dump($result);
 
 	}
 
